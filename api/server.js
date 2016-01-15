99b8f03f77d00fa82f6c68c15eee243eecf41616
MattBelangerBS <mbelanger@brainstation.io>
2016-01-15 13:26:32 -0500

    strut

---
+++ api/server.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+
+
+var app = express();
+
+app.use(express.static(__dirname + './../app/'));
+
+
+
+app.listen(8080,function(){
+		console.log('Listening on http://127.0.0.1:%s',"8080");
+		console.log('Stop Server With CTRL + C');
+	});
---
+++ app/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('subReddit', ['ui.router','ui.bootstrap','ngAnimate']);
+
+
+
+app.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+    .state('home', {
+        url: '/home',
+        views: {
+                content: {
+                    templateUrl: 'templates/home.html',
+                    controller: 'MainCtrl as Ctrl'
+                }
+
+        }  
+
+    })
+
+});
+
+
