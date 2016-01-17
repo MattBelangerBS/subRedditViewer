e3f03a82ae209852bea140c05919309b29594b35
Your Name <you@example.com>
2016-01-17 14:31:28 -0500

    qwef

--- app/js/controllers/main.ctrl.js
+++ app/js/controllers/main.ctrl.js
@@ -3,7 +3,31 @@ app.controller('MainCtrl',MainCtrl);
 function MainCtrl($state){
 
 	this.dog;
-
-
+	this.test1;
+	this.search;
+	
 }
+
+
+
+MainCtrl.prototype.click = function(search) {
+	var that = this;
+
+	reddit.hot(search).limit(5).fetch(function(res) {
+
+    // res contains JSON parsed response from Reddit
+    	console.log(res);
+    	that.test1 = res.data.children;
+    	console.log(that.test1);
+
+    	});
+    		
+};
+
+
+
+
+
+
+
 
