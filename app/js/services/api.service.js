84956ff8a29cb07f4b8cab6ae157b4fdb87876e7
Your Name <you@example.com>
2016-01-28 16:59:35 -0500

    v1.2

---
+++ app/js/services/api.service.js
@@ -0,0 +1,15 @@
+app.service('apiSrv',apiSrv);
+
+function apiSrv($http){
+	this.http = $http;
+
+	this.BASEURL= "http://reddit.com/r/";
+	this.ENDPOINT= "/.json";
+	
+}
+
+apiSrv.prototype.getRequest = function(sub) {
+
+		return this.http.get(this.BASEURL + sub+ this.ENDPOINT);
+	
+}
