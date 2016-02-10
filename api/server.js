a109a7fd4562d92e38b88355286c089ca670e1e3
Your Name <you@example.com>
2016-02-10 09:25:28 -0500

    aws

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + './../app/'));
 
 
 
-app.listen(8081,function(){
-		console.log('Listening on http://127.0.0.1:%s',"8081");
+app.listen(80,function(){
+		console.log('Listening on http://127.0.0.1:%s',"80");
 		console.log('Stop Server With CTRL + C');
 	});
